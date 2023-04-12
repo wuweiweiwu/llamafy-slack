31a3be754bc96e7f77b5c9fc9bae2bcffd005f7a
wuweiweiwu <hello@weiweiwu.me>
2023-04-12 15:52:55 -0700

    basic response works!

--- app.py
+++ app.py
@@ -10,6 +10,27 @@ load_dotenv()
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+
+# Listens to incoming messages that contain "hello"
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click Me"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!",
+    )
+
+
 # Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
