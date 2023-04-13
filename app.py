cff7856dca70aa8c84d339642325976053aa2c82
wuweiweiwu <hello@weiweiwu.me>
2023-04-12 17:39:40 -0700

    reply in thread

--- app.py
+++ app.py
@@ -12,23 +12,34 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 
 # Listens to incoming messages that contain "hello"
-@app.message("hello")
-def message_hello(message, say):
+@app.event("app_mention")
+def handle_mentions(event, client, say):
+    thread_ts = event.get("thread_ts", None) or event["ts"]
+
     # say() sends a message to the channel where the event was triggered
     say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click",
-                },
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!",
+        # blocks=[
+        #     {
+        #         "type": "section",
+        #         "text": {"type": "mrkdwn", "text": f"Hey there <@{event['user']}>!"},
+        #         "accessory": {
+        #             "type": "button",
+        #             "text": {"type": "plain_text", "text": "Click Me"},
+        #             "action_id": "button_click",
+        #             "value": thread_ts,
+        #         },
+        #     }
+        # ],
+        text=f"Hey there <@{event['user']}>!",
+        thread_ts=thread_ts,
     )
+
+
+# @app.action("button_click")
+# def action_button_click(body, ack, say):
+#     # Acknowledge the action
+#     ack()
+#     say(f"<@{body['user']['id']}> clicked the button", thread_ts=body["value"])
 
 
 # Start your app
