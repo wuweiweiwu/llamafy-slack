9a164cfbf509dc792e8e867fd7cb47d185416a95
wuweiweiwu <hello@weiweiwu.me>
2023-04-12 18:08:44 -0700

    openai call fun

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
 import os
 import re
+import time
 
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from dotenv import load_dotenv
+import openai
 
 # Load default environment variables (.env)
 load_dotenv()
+
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+
+def openai_call(
+    prompt: str,
+    model: str = "gpt-3.5-turbo",
+    temperature: float = 0.5,
+    max_tokens: int = 100,
+):
+    while True:
+        try:
+            # Use chat completion API
+            messages = [{"role": "system", "content": prompt}]
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens,
+                n=1,
+                stop=None,
+            )
+
+            return response.choices[0].message.content.strip()
+        except openai.error.RateLimitError:
+            print(
+                "The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again."
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        else:
+            break
+
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
