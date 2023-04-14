614e5b739c3c013ddd50107a88a9d61f0e1c6bab
wuweiweiwu <hello@weiweiwu.me>
2023-04-14 15:32:15 -0700

    initial generation works

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import re
 import time
 import json
 from typing import Dict, List
+from collections import OrderedDict
 
 from sqlalchemy import (
     MetaData,
@@ -34,6 +35,16 @@ openai.api_key = OPENAI_API_KEY
 # global engine
 # needs to be dynamic in the future
 ENGINE = create_engine("sqlite:///./chinook.db")
+
+DIALECT = "sqlite"
+
+MSG_WITH_ERROR_TRY_AGAIN = (
+    "Try again. "
+    f"Only respond with valid {DIALECT}. Write your answer in JSON. "
+    f"The {DIALECT} query you just generated resulted in the following error message:\n"
+    "{error_message}"
+    "Check the table schema and ensure that the columns for the table exist and will provide the expected results."
+)
 
 
 def get_assistant_message(
@@ -72,7 +83,23 @@ def extract_text_from_markdown(text: str) -> str:
     return text
 
 
-def get_table_indexes(inspector: Inspector, table: Table) -> str:
+def extract_sql_query_from_message(text: str) -> Dict:
+    try:
+        data = json.loads(text)
+    except Exception as e:
+        print("e: ", e)
+        raise e
+
+    if data.get("MissingData"):
+        return data
+
+    sql = data["SQL"]
+
+    return {"SQL": sql}
+
+
+def get_table_indexes(table: Table) -> str:
+    inspector = inspect(ENGINE)
     indexes = inspector.get_indexes(table.name)
     indexes_formatted = "\n".join(map(format_index, indexes))
     return f"Table Indexes:\n{indexes_formatted}"
@@ -109,23 +136,24 @@ def get_sample_rows(table: Table) -> str:
     )
 
 
-def get_table_info() -> str:
+def get_table_info(table_names: List[str] = None) -> str:
     """
     Get table info from the database
+    CREATE statements
     https://github.com/hwchase17/langchain/blob/634358db5e9d0f091c66c82b8ed1379ec6531f88/langchain/sql_database.py#L128
     """
     inspector = inspect(ENGINE)
     metadata = MetaData()
     metadata.reflect(bind=ENGINE)
 
-    table_names = inspector.get_table_names()
-    dialect = "sqlite"
+    if not table_names:
+        table_names = inspector.get_table_names()
 
     meta_tables = [
         tbl
         for tbl in metadata.sorted_tables
         if tbl.name in set(table_names)
-        and not (dialect == "sqlite" and tbl.name.startswith("sqlite_"))
+        and not (DIALECT == "sqlite" and tbl.name.startswith("sqlite_"))
     ]
 
     tables = []
@@ -135,79 +163,101 @@ def get_table_info() -> str:
         table_info = f"{create_table.rstrip()}"
 
         # extra info
-        table_info += "\n\n/*"
-        # table_info += f"\n{get_table_indexes(inspector,table)}\n"
-        table_info += f"\n{get_sample_rows(table)}\n"
-        table_info += "*/"
+        # table_info += "\n\n/*"
+        # table_info += f"\n{get_table_indexes(table)}\n"
+        # table_info += f"\n{get_sample_rows(table)}\n"
+        # table_info += "*/"
 
         tables.append(table_info)
 
-    # final_str = "\n\n".join(tables)
-    # return final_str
+    final_str = "\n\n".join(tables)
+    return final_str
 
-    return "\n\n".join(table_names)
-
-
-def get_table_selection_messages():
-    """
-    system messages and few shot examples
-
-    """
-    # default_messages = [
-    #     {
-    #         "role": "system",
-    #         "content": (
-    #             "You are a helpful assistant for identifying relevant SQL tables to use for answering a natural language query."
-    #             ' You respond in JSON format with your answer in a field named "tables" which is a list of strings.'
-    #             " Respond with an empty list if you cannot identify any relevant tables."
-    #             # " Write your answer in markdown format."
-    #             "\n"
-    #             "The following are descriptions of available tables and enums:\n"
-    #             "---------------------\n" + get_table_info() + "---------------------\n"
-    #         ),
-    #     }
-    # ]
-
-    # default_messages.extend([
-
-    # ])
-    return []
+    # return "\n\n".join(table_names)
 
 
-def get_table_selection_user_message_with_descriptions(
+def execute_sql(sql_query: str) -> Dict:
+    with ENGINE.connect() as connection:
+        with connection.begin():
+            sql_text = text(sql_query)
+            result = connection.execute(sql_text)
+
+        column_names = list(result.keys())
+        rows = [list(r) for r in result.all()]
+
+        results = []
+        for row in rows:
+            result = OrderedDict()
+            for i, column_name in enumerate(column_names):
+                result[column_name] = row[i]
+            results.append(result)
+
+        return {
+            "column_names": column_names,
+            "results": results,
+        }
+
+
+# def get_table_selection_messages():
+#     """
+#     system messages and few shot examples
+
+#     """
+#     # default_messages = [
+#     #     {
+#     #         "role": "system",
+#     #         "content": (
+#     #             "You are a helpful assistant for identifying relevant SQL tables to use for answering a natural language query."
+#     #             ' You respond in JSON format with your answer in a field named "tables" which is a list of strings.'
+#     #             " Respond with an empty list if you cannot identify any relevant tables."
+#     #             # " Write your answer in markdown format."
+#     #             "\n"
+#     #             "The following are descriptions of available tables and enums:\n"
+#     #             "---------------------\n" + get_table_info() + "---------------------\n"
+#     #         ),
+#     #     }
+#     # ]
+
+#     # default_messages.extend([
+
+#     # ])
+#     return []
+
+
+def get_table_selection_prompt(
     natural_language_query: str,
 ) -> str:
     message = f"""
-        You are an expert data scientist.
-        Return a JSON object with relevant SQL tables for answering the following natural language query:
-        ---------------
-        {natural_language_query}
-        ---------------
-        Respond in JSON format with your answer in a field named \"tables\" which is a list of strings.
-        Respond with an empty list if you cannot identify any relevant tables.
-        Write your answer in markdown format.
-        """
+You are an expert data scientist.
+Return a JSON object with relevant SQL tables for answering the following natural language query:
+---------------
+{natural_language_query}
+---------------
+Respond in JSON format with your answer in a field named \"tables\" which is a list of strings.
+Respond with an empty list if you cannot identify any relevant tables.
+Write your answer in markdown format.
+"""
 
     return (
         message
         + f"""
-        The following are the scripts that created the tables as well as sample rows that that table:
-        ---------------------
-        {get_table_info()}
-        --------------------- 
+The following are the scripts that created the tables:
+---------------------
+{get_table_info()} 
+--------------------- 
 
-        in your answer, provide the following information:
-        
-        - <one to two sentence comment explaining what tables can be relevant goes here>
-        - <for each table identified, comment double checking the table is in the schema above along with what the first column in the table is or (none) if it doesn't exist. be careful that any tables suggested were actually above>
-        - <if any tables were incorrectly identified, make a note here about what tables from the schema should actually be used if any>
-        - the markdown formatted like this:
-        ```
-        <json of the tables>
-        ```
+in your answer, provide the following information:
 
-        Thanks!
-        """
+- <one to two sentence comment explaining what tables can be relevant goes here>
+- <for each table identified, comment double checking the table is in the schema above along with what the first column in the table is or (none) if it doesn't exist. be careful that any tables suggested were actually above>
+- <if any tables were incorrectly identified, make a note here about what tables from the schema should actually be used if any>
+- the markdown formatted like this:
+```
+<json of the tables>
+```
+
+Thanks!
+"""
     )
 
 
@@ -216,22 +266,144 @@ def get_relevant_tables(natural_language_query: str) -> List[str]:
     Identify relevant tables for answering a natural language query via LM
     """
 
-    messages = get_table_selection_messages().copy()
+    # messages = get_table_selection_messages().copy()
+    messages = []
 
-    user_content = get_table_selection_user_message_with_descriptions(
+    prompt = get_table_selection_prompt(
         natural_language_query=natural_language_query,
     )
-    messages.append({"role": "user", "content": user_content})
+    messages.append({"role": "user", "content": prompt})
 
     assistant_message = get_assistant_message(
         messages=messages,
         model="gpt-3.5-turbo",
     )["message"]["content"]
 
+    print(assistant_message)
+
     tables_json_str = extract_text_from_markdown(assistant_message)
     tables = json.loads(tables_json_str).get("tables")
 
     return tables
+
+
+# def get_rephrase_prompt(
+#     natural_language_query: str,
+#     table_info: str,
+# ):
+#     return f"""
+# Let's start by rephrasing the query to be more analytical. Use the schema context to rephrase the user question in a way that leads to optimal query results: {natural_language_query}
+# The following are schemas of tables you can query as well as sample rows in that table:
+# ---------------------
+# {table_info}
+# ---------------------
+
+# Do not include any of the table names in the query.
+# Ask the natural language query the way a data analyst, with knowledge of these tables, would.
+# """
+
+
+def get_retry_prompt(natural_language_query: str, table_info: str) -> str:
+    return f"""You are an expert and empathetic database engineer that is generating correct read-only {DIALECT} query to answer the following question/command: {natural_language_query}
+
+We already created the tables in the database with the following CREATE TABLE code:
+---------------------
+{table_info}
+---------------------
+
+Ensure to include which table each column is from (table.column)
+Use CTE format for computing subqueries.
+
+Provide a properly formatted JSON object with the following information. Ensure to escape any special characters so it can be parsed as JSON.
+
+{{
+    "Schema": "<1 to 2 sentences about the tables/columns/enums above to use>",
+    "Applicability": "<1 to 2 sentences about which columns and enums are relevant, or which ones are missing>",
+    "SQL": "<your query>"
+}}
+
+However, if the tables don't contain all the required data (e.g. the column isn't there or there aren't relevant enums), instead return a JSON object with just: 
+{{
+    "Schema": "<1 to 2 sentences about the tables/columns/enums above to use>",
+    "Applicability": "<1 to 2 sentences about which columns and enums are relevant, or which ones are missing>",
+    "MissingData": "<1 to 2 sentences about what data is missing>"
+}}
+However, if a query can be close enough to the intent of the question/command, generate the SQL that gets it instead of returning MissingData.
+"""
+
+
+def text_to_sql_with_retry(
+    natural_language_query: str,
+    table_names: List[str],
+    k=1,
+):
+    """
+    Tries to take a natural language query and generate valid SQL to answer it K times
+    """
+    # ask the assistant to rephrase before generating the query
+    table_info = get_table_info(table_names)
+
+    # rephrase = [{
+    #     "role": "user",
+    #     "content": make_rephrase_msg_with_schema_and_warnings().format(
+    #         natural_language_query=natural_language_query,
+    #         schemas=schemas
+    #         )
+    # }]
+    # rephrased_query = get_assistant_message(rephrase)["message"]["content"]
+    # print(f'[REPHRASED_QUERY] {rephrased_query}')
+    # natural_language_query=rephrased_query
+
+    content = get_retry_prompt(natural_language_query, table_info)
+
+    # messages = make_default_messages(schemas, scope)
+    messages = []
+    messages.append({"role": "user", "content": content})
+
+    assistant_message = None
+
+    for _ in range(k):
+        sql_query_data = {}
+        try:
+            assistant_message = get_assistant_message(
+                messages,
+                model="gpt-3.5-turbo",
+            )[
+                "message"
+            ]["content"]
+
+            print(assistant_message)
+
+            sql_query_data = extract_sql_query_from_message(assistant_message)
+
+            if sql_query_data.get("MissingData"):
+                return {"MissingData": sql_query_data["MissingData"]}, ""
+
+            sql_query = sql_query_data["SQL"]
+
+            response = execute_sql(sql_query)
+
+            # Generated SQL query did not produce exception. Return result
+            return response, sql_query
+
+        except Exception as e:
+            print("failed to execute sql query", e)
+
+            messages.append(
+                {
+                    "role": "assistant",
+                    "content": assistant_message["message"]["content"],
+                }
+            )
+            messages.append(
+                {
+                    "role": "user",
+                    "content": MSG_WITH_ERROR_TRY_AGAIN.format(error_message=str(e)),
+                }
+            )
+
+    print(f"Could not generate {DIALECT} query after {k} tries.")
+    return None, None
 
 
 # Initializes your app with your bot token and socket mode handler
@@ -270,6 +442,15 @@ def handle_mentions(event, client, say):
 
 # Start your app
 if __name__ == "__main__":
-    print(get_relevant_tables("what are the top 10 albums"))
+    # print(get_table_info())
+
+    question = "Who are the top 3 best selling artists?"
+
+    # tables = get_relevant_tables(question)
+
+    tables = ["invoices", "invoice_items", "tracks", "albums"]
+
+    print(text_to_sql_with_retry(question, tables))
+
     # print(get_table_info())
     # SocketModeHandler(app, SLACK_APP_TOKEN).start()
